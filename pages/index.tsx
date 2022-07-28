4ce945970aeefdc733acb888447050b39475abd7
Egor <znogoud3@gmail.com>
2022-07-28 19:50:08 +0200

    Main Video View

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,32 @@
 import type { NextPage } from 'next';
+import axios from 'axios';
+import { Video } from '../types';
+import VideoCard from '../components/VideoCard';
+import NoResults from '../components/NoResults';
+interface IProps {
+  videos: Video[];
+}
 
-const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+const Home = ({ videos }: IProps) => {
+  return (
+    <div className="flex flex-col gap-10 videos h-full">
+      {videos.length ? (
+        videos.map((video: Video) => <VideoCard post={video} key={video._id} />)
+      ) : (
+        <NoResults text={'No Videos'} />
+      )}
+    </div>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
+
+  return {
+    props: {
+      videos: data,
+    },
+  };
 };
 
 export default Home;
--- utils/client.ts
+++ utils/client.ts
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client';
 
 export const client = sanityClient({
-  projectId: 'tp4ln3j8',
+  projectId: 'kbt9qzul',
   dataset: 'production',
   apiVersion: '2022-03-10',
   useCdn: false,
